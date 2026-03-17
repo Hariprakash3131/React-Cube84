389dcc5ba0037916468638c2b3f4e19b8d8cc41d
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-17 13:19:31 +0530

    react-1

---
+++ FirstReact/firstExample/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  let name = "Sarath";
+
+  return (
+    <>
+      {/* Task 1 */}
+      <h1>Welcome to React</h1>
+      <p>Learning Day 1</p>
+
+      {/* Task 2 */}
+      <h1>Hello {name}</h1>
+
+      {/* Task 3 */}
+      <h1>Lorem ipsum dolor sit amet</h1>
+      <p>Lorem ipsum dolor sit.</p>
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default App
