ca4b04ba354c49befdcf887f7279c27ddc415bdd
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-24 17:58:52 +0530

    react-10

---
+++ day-2/src/about/aboutPage.jsx
@@ -0,0 +1,8 @@
+function AboutPage(){
+    return(
+        <div>
+            <h1>This is AboutPage</h1>
+        </div>
+    )
+}
+export default AboutPage
---
+++ day-2/src/app/contact/page.js
@@ -0,0 +1,5 @@
+import ContactPage from '@/contact/contactPage';
+
+export default function Contact() {
+  return <ContactPage />;
+}
