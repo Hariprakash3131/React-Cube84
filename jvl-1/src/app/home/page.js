76d9a1e592bfb6708cab37a3a2d843c2030e1f30
Hariprakash3131 <hariprakash3131@gmail.com>
2026-04-14 19:18:13 +0530

    react-34

---
+++ jvl-1/src/app/home/page.js
@@ -0,0 +1,10 @@
+export default function frontPage(){
+    return(
+        <div>
+            <h1>
+                Front Page
+            </h1>
+        </div>
+    )
+}
+
---
+++ jvl-1/src/app/routeSegments/settings/page.js
@@ -0,0 +1,8 @@
+export default function page(){
+    return(
+        <div>
+            <h1>Route Segments</h1>
+        </div>
+    )
+
+}
