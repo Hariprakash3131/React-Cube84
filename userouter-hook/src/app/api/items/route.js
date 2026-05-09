c9445b18153fdd813a9a3fe304292e24cfdb119f
Hariprakash3131 <hariprakash3131@gmail.com>
2026-05-09 15:11:59 +0530

    react-42

--- userouter-hook/src/app/api/items/route.js
+++ userouter-hook/src/app/api/items/route.js
@@ -1,4 +1,9 @@
-export async function GET() {
-    return new Response('Test')
+// export async function GET() {
+//     return new Response('Test')
     
-}
+// }   its a string a data
+
+export async function GET() {
+    return Response.json([{'Name':'Saratha'},{'Name':'Surya'}])
+    
+}   //This is a Json data
