e0152aa62848a76165c167ff4fdfef63bfd3a436
Hariprakash3131 <hariprakash3131@gmail.com>
2026-05-12 22:53:21 +0530

    react-49

--- blog-project/src/components/header.js
+++ blog-project/src/components/header.js
@@ -3,8 +3,8 @@ export default function Header(){
 
        <header className="flex justify-between items-center px-6 bg-white rounded-xl border border-gray-200 h-18">
         <nav className="flex gap-8 items-center">
-             <a href="#" className="pb-1 text-sm font-medium text-gray-800 border-b-2 transition-colors hover:text-black border-transprent hover:border-black">Home</a>
-             <a href="#" className="pb-1 text-sm font-medium text-gray-800 border-b-2 transition-colors hover:text-black border-transprent hover:border-black" >Artical</a>
+             <a href="#" className="pb-1 text-sm font-medium text-gray-600 border-b-2 transition-colors hover:text-black border-transprent hover:border-black">Home</a>
+             <a href="#" className="pb-1 text-sm font-medium text-gray-600 border-b-2 transition-colors hover:text-black border-transprent hover:border-black" >Artical</a>
         </nav>
         <div className="flex gap-8 items-center font-medium">
             <div>
